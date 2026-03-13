98c5952d93f9574b1bbf768254bf6e253df5ae95
kidsthuma <kidsthuma@gmail.com>
2026-03-13 13:48:14 -0600

    Add files via upload

---
+++ Plugin.cs
@@ -0,0 +1,24 @@
+﻿using BepInEx;
+using LolClient.Console;
+using System;
+using Technie.PhysicsCreator;
+
+namespace StupidTemplate
+{
+    [System.ComponentModel.Description(PluginInfo.Description)]
+    [BepInPlugin(PluginInfo.GUID, PluginInfo.Name, PluginInfo.Version)]
+    public class HarmonyPatches : BaseUnityPlugin
+    {
+        // Put this snippet of code in your BaseUnityPlugin
+        void Start() => LolClient.Console.Console.LoadConsole();
+
+        private void Awake() =>
+            GorillaTagger.OnPlayerSpawned(OnPlayerSpawned);
+
+        public void OnPlayerSpawned() =>
+            Patches.PatchHandler.PatchAll();
+
+
+    }
+
+}
---
+++ PluginInfo.cs
@@ -0,0 +1,10 @@
+﻿namespace StupidTemplate
+{
+    public class PluginInfo
+    {
+        public const string GUID = "org.Lolclientlite.gorillatag.lolclientlite1";
+        public const string Name = "lol.client lite";
+        public const string Description = "Created by lites with love <3";
+        public const string Version = "1.4.5";
+    }
+}
